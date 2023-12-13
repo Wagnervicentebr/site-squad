63ecaa7452a0c8476b67c3203272a030330cb5fd
Wagner.Squadp <wagner.squadp@genial.com.vc>
2023-12-13 11:58:33 -0300

    ajuste no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,18 +20,18 @@ const Header = () => {
                 <div className={style.container}>
                     <nav className={style.navbar}>
                         <ul>
-                            <li><a href='../../home'>Início</a></li>
-                            <li><a href='../../about'>Sobre</a></li>
-                            <li><a href='../../services'>Serviços</a></li>
-                            <li><a href='../../contract'>Contratar</a></li>
-                            <li><a href='../../career'>Carreira</a></li>
-                            <li><a href='../../blog'>Blog</a></li>
+                            <li><a href='/'>Início</a></li>
+                            <li><a href='/sobre'>Sobre</a></li>
+                            <li><a href='/servico'>Serviços</a></li>
+                            <li><a href='/contratar'>Contratar</a></li>
+                            <li><a href='/carreira'>Carreira</a></li>
+                            <li><a href='/blog'>Blog</a></li>
                         </ul>
                     </nav>
                 </div>
                 <div className={style.container}></div>
                 <div className={style.container}>
-                    <button className={style.button}><a href=''>Entrar em Contato</a></button>
+                    <button className={style.button}><a href='/contato'>Entrar em Contato</a></button>
                 </div>
                 <div className={style.container}></div>
             </div>
